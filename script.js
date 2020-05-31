568e4a1c9119ad987b9dc70747e96fed8fa3275b
Ravinder Deol <ravinder.r.deol@gmail.com>
2020-05-31 17:52:44 +0100

    generates random users

---
+++ script.js
@@ -0,0 +1,34 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionaires = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Feth Random User & Add Money
+
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    }
+
+    addData(newUser);
+}
+
+// Add New Obeject To Data Array
+
+function addData(obj) {
+    data.push(obj);
+}
